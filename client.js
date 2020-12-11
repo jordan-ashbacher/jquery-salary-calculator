d432bd5bc73bea435ee208606071870231fd9bcc
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2020-12-11 14:36:14 -0600

    creates html framework for salary calculator with inputs, submit button and table. sets up handleReady function with renderDOM framework, submit event listener and handleSubmit function framework.

--- client.js
+++ client.js
@@ -1,7 +1,34 @@
 console.log('js linked')
+
+let employees = []
+
 $(document).ready(handleReady)
 
 function handleReady() {
     console.log('jq linked');
+
+    renderDom()
+
+    //event listeners
+    $('submit-button').on('click', handleSubmit)
     
+}
+
+function renderDOM() {
+
+    $('employee-info').empty()
+
+    for (let employee of employees) {
+
+    }
+
+}
+
+function handleSubmit() {
+    console.log('submit button clicked')
+
+    //create new employee object from input fields
+    let newEmployee = {
+        firstName: 
+    }
 }
